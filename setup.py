2032ce3460bfbbf8e4332cfb7cc04ee5f76af2b8
Steven Sierra <bizoru@users.noreply.github.com>
2018-09-20 01:35:06 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gitswitch",
-    version="0.0.1",
+    version="0.0.2",
     author="Steven Sierra",
     author_email="bizoru@gmail.com",
     description="A small utility to switch different ssh folders",
